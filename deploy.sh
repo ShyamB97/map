54ff8aff720e87fd135fbc348845877feddce1b9
ShyamB97 <bhuller.shyam@gmail.com>
2025-05-18 18:36:13 +0100

    auto deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# how deploy the app on the github page (assuming my page points to this repo still).
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# 1. generate the new index html. Pass the map.py args through this script
+python map.py $1 --export_html
+mv map.html index.html
+# 2. in a temporary folder, clone the deploy branch
+cd tmp
+git clone -b deploy https://github.com/ShyamB97/map.git
+# 3. copy the new index.html into depoly
+cp $SCRIPT_DIR/index.html .
+# 4. push deploy.
+git commit -a -m "update deploy html"
+git push
+# 5. wait
